505e8765be7d0c0576c1e073aad4b2c292771ad2
dc-choi <57163578+dc-choi@users.noreply.github.com>
2020-03-19 13:40:17 +0900

    Add files via upload

---
+++ vo/AddressAndCityAndCountry.java
@@ -0,0 +1,26 @@
+package vo;
+
+public class AddressAndCityAndCountry {
+	private City city;
+	private Country country;
+	private Address address;
+	   
+	public City getCity() {
+	   return city;
+	}
+	public void setCity(City city) {
+	   this.city = city;
+	}
+	public Country getCountry() {
+	   return country;
+	}
+	public void setCountry(Country country) {
+	   this.country = country;
+	}
+	public Address getAddress() {
+	   return address;
+	}
+	public void setAddress(Address address) {
+	   this.address = address;
+	}
+}
---
+++ vo/CityAndCountry.java
@@ -0,0 +1,20 @@
+package vo;
+
+public class CityAndCountry {
+   private City city;
+   private Country country;
+   
+   public City getCity() {
+      return this.city;
+   }
+   public void setCity(City city) {
+      this.city = city;
+   }
+   
+   public Country getCountry () {
+      return this.country;
+   }
+   public void setCountry(Country country) {
+      this.country = country;
+   }
+}
