aef16689375b62d036a18716b16a3669305c0452
chonghg21 <62366977+chonghg21@users.noreply.github.com>
2020-03-19 13:26:40 +0900

    Create Actor.java

---
+++ Actor.java
@@ -0,0 +1,41 @@
+package vo;
+
+public class Actor {
+	private int actorId;
+	
+	public void setActorId(int actorId) {
+	this.actorId = actorId;
+	}
+	
+	public int getActorId() {
+		return this.actorId;
+	}
+	
+	private String firstName;
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	private String lastName;
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLastName() {
+		return this.lastName;
+	}
+	private String lastUpdate;
+	
+	public void setLastUpdate(String lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public String getLastUpdate() {
+		return this.lastUpdate;
+	}
+	
+	
+}
